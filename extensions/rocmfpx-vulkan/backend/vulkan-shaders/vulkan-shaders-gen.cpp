7c4b5c0e50f9a3228f2b53b12764165dcb3e61ac
isaacalbala12 <isaacalbala12@users.noreply.github.com>
2026-09-18 16:06:13 +0200

    Revert "perf: prototype dual accumulators for gate N4"
    This reverts commit 150396a2148cbbff5a91267447dbdbd2953f4326.

--- extensions/rocmfpx-vulkan/backend/vulkan-shaders/vulkan-shaders-gen.cpp
+++ extensions/rocmfpx-vulkan/backend/vulkan-shaders/vulkan-shaders-gen.cpp
@@ -759,13 +759,6 @@ void process_shaders() {
             string_to_spv("mul_mat_vec_id_" + tname + "_q8_1_f32", "mul_mat_vecq.comp", merge_maps(base_dict, {{"MUL_MAT_ID", "1"}, {data_a_key, "1"}, {"D_TYPE", "float"}, {"FLOAT_TYPE", "float"}, {"FLOAT_TYPEV2", "vec2"}, {"ACC_TYPE", "float"}}));
             string_to_spv("mul_mat_vec_id_" + tname + "_q8_1_f32_subgroup", "mul_mat_vecq.comp", merge_maps(base_dict, {{"MUL_MAT_ID", "1"}, {data_a_key, "1"}, {"D_TYPE", "float"}, {"FLOAT_TYPE", "float"}, {"FLOAT_TYPEV2", "vec2"}, {"ACC_TYPE", "float"}, {"USE_SUBGROUP_ADD", "1"}}));
             string_to_spv("mul_mat_vec_id_" + tname + "_q8_1_f32_subgroup_no_shmem", "mul_mat_vecq.comp", merge_maps(base_dict, {{"MUL_MAT_ID", "1"}, {data_a_key, "1"}, {"D_TYPE", "float"}, {"FLOAT_TYPE", "float"}, {"FLOAT_TYPEV2", "vec2"}, {"ACC_TYPE", "float"}, {"USE_SUBGROUP_ADD_NO_SHMEM", "1"}}));
-
-            if (tname == "rocmfp4_fast") {
-                string_to_spv("mul_mat_vec_rocmfp4_fast_dualacc_q8_1_f32_subgroup", "mul_mat_vecq.comp",
-                    merge_maps(base_dict, {{data_a_key, "1"}, {"D_TYPE", "float"}, {"FLOAT_TYPE", "float"},
-                        {"FLOAT_TYPEV2", "vec2"}, {"ACC_TYPE", "float"}, {"USE_SUBGROUP_ADD", "1"},
-                        {"ROCMFP4_FAST_DUAL_ACCUM", "1"}}));
-            }
         }
 #endif
 
