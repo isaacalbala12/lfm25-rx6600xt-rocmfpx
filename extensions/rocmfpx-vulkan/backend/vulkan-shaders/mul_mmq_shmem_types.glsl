f7a53ab159e25bad525ea5b3cdbea9101de82a26
isaacalbala12 <isaacalbala12@users.noreply.github.com>
2026-09-18 05:21:06 +0200

    revert: reject global FP4 FAST BK step 2

--- extensions/rocmfpx-vulkan/backend/vulkan-shaders/mul_mmq_shmem_types.glsl
+++ extensions/rocmfpx-vulkan/backend/vulkan-shaders/mul_mmq_shmem_types.glsl
@@ -61,9 +61,6 @@ struct block_a_cache {
 };
 #elif defined(DATA_A_ROCMFP4_FAST)
 #define QUANT_R_MMQ 2
-#ifndef MUL_MAT_ID
-#define BK_STEP 2
-#endif
 struct block_a_cache {
     int32_t qs[8];
     FLOAT_TYPE d;
